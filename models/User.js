73312963275ab1ed1ba12f347cc69472a528a9bf
zarin-anjum <zarinanjum0801@gmail.com>
2026-05-14 21:52:12 +0600

    added user-model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    photo: {
+      type: String,
+      default: "",
+    },
+    role: {
+      type: String,
+      enum: ["user", "creator", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
