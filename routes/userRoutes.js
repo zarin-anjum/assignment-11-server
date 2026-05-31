42c5bfb14d06d1d7f619ee629365a03031bebcba
zarin-anjum <zarinanjum0801@gmail.com>
2026-05-31 13:40:27 +0600

    updated user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import verifyToken from "../middleware/verifyToken.js";
+import Contest from "../models/Contest.js";
 
 const router = express.Router();
 
@@ -108,6 +109,20 @@ router.patch("/me", verifyToken, async (req, res) => {
       { new: true },
     );
     res.status(200).json(updated);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get("/leaderboard", async (req, res) => {
+  try {
+    const winners = await Contest.aggregate([
+      { $match: { "winner.email": { $ne: null } } },
+      { $group: { _id: "$winner.email", wins: { $sum: 1 }, name: { $first: "$winner.name" }, photo: { $first: "$winner.photo" } } },
+      { $sort: { wins: -1 } },
+      { $limit: 20 },
+    ]);
+    res.status(200).json(winners);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
