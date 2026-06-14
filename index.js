c900b013abbd73c5c2f8642712aa135169a40505
zarin-anjum <zarinanjum0801@gmail.com>
2026-06-14 16:15:39 +0600

    Update CORS origin with new client URL

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "https://contesthub-zarin.netlify.app",
+      "https://upgrade-contesthub-zarin.netlify.app",
     ],
     credentials: true,
   }),
